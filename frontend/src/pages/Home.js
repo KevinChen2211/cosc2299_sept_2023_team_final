06bb0657aa13f06a00c6891c89a4f1d49f6f7701
s3780646 <s3780646@student.rmit.edu.au>
2023-09-15 00:22:49 +1000

    Task 50 add home menu with search bar

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,14 +1,27 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import SearchBar from "./components/SearchBar";
+import supermarket from "../assets/supermarket.png"
 
 function Home(props) {
-  const navigate = useNavigate();
-  const handleSubmit = (event) => {
 
+  // Get an array of
+  const images = ['apple.png', 'milk.png', 'bread.png', 'water.png'];
+
+
+  const navigate = useNavigate();
+
+  const handleImageClick = (product) => {
+    // Replace with the actual URLs with the navigate 
+    navigate("/product/" + product, { state: { productName: product } });
+    return;
+  }
+
+
+
+  const handleSubmit = (event) => {
     navigate("/signup");
     return;
-
-
   }
   return (
     <div className="text-center">
@@ -25,8 +38,35 @@ function Home(props) {
         </>
         :
         <>
-          <p>you only see this when you log in</p>
+          <div className="home-page-search-catagories">
+            {/* TODO: add feature to broswe tab */}
+            <button>browse</button>
+            <SearchBar />
+          </div>
+          <img
+            src={supermarket}
+            className="main-page-image"
+          />
+          <h1>specials</h1>
+          <div className="specials-container">
+            {images
+              .map((image, index) => (
+                <div key={index}>
+                  <img
+                    key={index}
+                    // this will need to change into an api call
+                    src={require(`./components/exampleimages/${image}`)}
+                    alt={image}
+                    onClick={() => handleImageClick(image)}
+                    style={{
+                      cursor: 'pointer',
+                    }}
+                  />
+                  <p>{image}</p>
+                </div>
+              ))}
 
+          </div>
         </>
       }
 
