a0401206bcce672e33861b007b56abd7d60607b5
[Tanya Tran] <[s3843142@student.rmit.edu.au]>
2023-09-16 01:56:32 +1000

    change nav bar items to image icons

--- frontend/src/fragments/Navbar.js
+++ frontend/src/fragments/Navbar.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/logo.png";
+// import logo from "../assets/logo.png";
+import bwlogo from "../assets/bwlogo.png";
+import logout from "../assets/logout.png";
+import location from "../assets/location.png";
+import notification from "../assets/notification.png";
+import profile from "../assets/profile.png";
+// import shoppingcart from "../assets/shoppingcart.png";
 
 // navigation bar component
 function Navbar(props) {
@@ -8,7 +14,7 @@ function Navbar(props) {
     // nav bar colour bootstrap
     <nav className="navbar navbar-expand-sm bg-warning navbar-dark">
       <div className="container">
-        <Link className="navbar-brand" to="/"><img src={logo} alt="logo" width="180" height="80" /></Link>
+        <Link className="navbar-brand" to="/"><img src={bwlogo} alt="bwlogo" width="200" height="70" /></Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -23,25 +29,24 @@ function Navbar(props) {
             {/* if a user is not logged in, they will see a Sign Up and Sign In page available in nav bar */}
             {props.email === null ?
               <>
-             
+
               </>
               :
               <>
-        
                 <li className="nav-item">
-                  <Link className="nav-link" to="/profile">Profile</Link>
+                  <Link className="nav-link" to="/location"> <img src={location} alt="location" width="30" height="30"/> </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/location">Location</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/notifications">Notifications</Link>
+                  <Link className="nav-link" to="/profile"><img src={profile} alt="profile" width="30" height="30"/></Link>
                 </li>
                 {/* <li className="nav-item">
-                  <Link className="nav-link" to="/shoppingcart">Shopping Cart</Link>
+                  <Link className="nav-link" to="/shoppingcart"><img src={shoppingcart} alt="shoppingcart" width="30" height="30"/></Link>
                 </li> */}
                 <li className="nav-item">
-                  <Link className="nav-link" to="/login" onClick={props.logoutUser}>Logout</Link>
+                  <Link className="nav-link" to="/notifications"><img src={notification} alt="notification" width="30" height="30"/></Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/login" onClick={props.logoutUser}><img src={logout} alt="logout" width="30" height="30"/></Link>
                 </li>
 
               </>
