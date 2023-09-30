5d9d74a1671b04fa919e2acc84131029af3bf60b
s3780646 <s3780646@student.rmit.edu.au>
2023-10-01 06:53:09 +1100

    Task 155 add template tests

---
+++ frontend/__tests__/LoggedIn.test.js
@@ -0,0 +1,17 @@
+import { describe, test, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+// import LoggedIn from '../src/pages/LoggedIn';
+
+
+describe('Home page', () => {
+    test('Displays header', () => {
+        // const wrapper = render(<LoggedIn />)
+        // expect(wrapper).toBeTruthy()
+
+        // Get by h1
+        // const h1 = wrapper.container.querySelector('h1')
+        // expect(h1?.textContent).toBe('Popular Items')
+        expect(true).toBe(true);
+    })
+});
+
