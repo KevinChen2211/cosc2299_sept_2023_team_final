d6f8d36694b5f589f219b921d3d4e35fdfe8c452
s3780646 <s3780646@student.rmit.edu.au>
2023-10-07 03:13:45 +1100

    Delivery page to be reactive

--- frontend/src/pages/Delivery.js
+++ frontend/src/pages/Delivery.js
@@ -1,10 +1,14 @@
 import '../styling.css';
-import { useNavigate } from "react-router-dom";
-import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import SwitchButton from './components/SwitchButton';
 
 
 export default function ShoppingCart({ cartItems }) {
     const navigate = useNavigate();
+    const location = useLocation();
+    const [deliveryFee, setDeliveryFee] = useState(7);
+
     const calculateTotal = () => {
         let total = 0;
         for (const item of cartItems) {
@@ -13,45 +17,78 @@ export default function ShoppingCart({ cartItems }) {
         return total.toFixed(2);
     }
 
-    const deliveryFee = 7;
+    function addDaysToDate(days) {
+        let today = new Date();
+        today.setDate(today.getDate() + days);
+
+        let date = today.getDate() + "-" + (today.getMonth() + 1) + "-" + today.getFullYear();
+
+        return date;
+    }
+
+    const handleFeeChange = (fee) => {
+        // Set the global variable to the input value
+        setDeliveryFee(fee);
+    };
+
     return (
-        <div align='center'>
-            <div align='left'>
-                <button onClick={() => navigate('/checkout')} >
-                    <span>
-                        Back to checkout
-                    </span>
+        <div align="center">
+            <div align="left">
+                <button onClick={() => navigate('/checkout')}>
+                    <span>Back to checkout</span>
                 </button>
             </div>
-            <h2>Select Delivery Option</h2><br />
-            <h5>Standard Delivery</h5>
-            <input type='radio' value='Standard Delivery' name='delivery' checked={true} />
-            <span> Delivered by: DD/MM/YY - DD/MM/YY </span>
-            <span>$ {deliveryFee}</span>
-            <br />Select time preference:
-            <button> 9AM - 1PM </button> <button> 1PM - 5PM </button>
-            <hr />
-
-            <h5>Express Delivery</h5>
-            <input type='radio' value='Standard Delivery' name='delivery' />
-            <span> Delivered by: DD/MM/YY - DD/MM/YY </span>
-            <span>$ {deliveryFee}</span>
-            <br />Select time preference:
-            <button> 9AM - 1PM </button> <button> 1PM - 5PM </button> <br /><br />
+            <h2>Select Delivery Option</h2>
+            <h3>delivering to {location.state.address}</h3>
+            <div className="standard-delivery">
+                <h5>Standard Delivery</h5>
+                <input
+                    type="radio"
+                    value="Standard Delivery"
+                    name="delivery"
+                    checked={deliveryFee === 7}
+                    onChange={() => handleFeeChange(7)}
+                />
+                <span> Delivered between: {addDaysToDate(7)} - {addDaysToDate(14)} </span>
+                <br />
+                <span>$7 delivery fee</span>
+                <br />
+                Select time preference: <SwitchButton />
+                <hr />
+            </div>
+            <div className="express-delivery">
+                <h5>Express Delivery</h5>
+                <input
+                    type="radio"
+                    value="Express Delivery"
+                    name="delivery"
+                    checked={deliveryFee === 14}
+                    onChange={() => handleFeeChange(14)}
+                />
+                <span> Delivered between: {addDaysToDate(1)} - {addDaysToDate(7)} </span>
+                <br />
+                <span>$14 delivery fee</span>
+                <br />
+                Select time preference: <SwitchButton />
+            </div>
+            <br />
+            <br />
             <hr />
             <div>Amount: ${calculateTotal()}</div>
-            <div>Total amount: ${(parseFloat(calculateTotal()) + deliveryFee).toFixed(2)}</div>
-
-            <input type='text' placeholder='Add a note ' />
-
-            <div className='form-group' align='center'>
+            <div>
+                Total amount: $
+                {(parseFloat(calculateTotal()) + deliveryFee).toFixed(2)}
+            </div>
+            <input type="text" placeholder="Add a note " />
+            <div className="form-group" align="center">
                 <br />
-                <button className='checkoutButton'
-                    onClick={() => navigate('/payment', { state: { deliveryFee: deliveryFee } })}>
+                <button
+                    className="checkoutButton"
+                    onClick={() => navigate('/payment', { state: { deliveryFee } })}
+                >
                     Continue to Payment
                 </button>
             </div>
-
         </div>
     );
 }
