0c5d38ab16950493b7a353728ad4a0fd242e2be8
s3780646 <s3780646@student.rmit.edu.au>
2023-10-06 21:31:37 +1100

    Task 68 change format

--- frontend/src/pages/Delivery.js
+++ frontend/src/pages/Delivery.js
@@ -1,20 +1,20 @@
 import '../styling.css';
-import { useNavigate } from "react-router-dom"; 
+import { useNavigate } from "react-router-dom";
 import React from "react";
 
 
-export default function ShoppingCart({cartItems}){
+export default function ShoppingCart({ cartItems }) {
     const navigate = useNavigate();
     const calculateTotal = () => {
         let total = 0;
         for (const item of cartItems) {
-          total += (item.price * item.boughtQuantity);
+            total += (item.price * item.boughtQuantity);
         }
         return total.toFixed(2);
-      }
+    }
 
     const deliveryFee = 7;
-  return (
+    return (
         <div align='center'>
             <div align='left'>
                 <button onClick={() => navigate('/checkout')} >
@@ -23,35 +23,35 @@ export default function ShoppingCart({cartItems}){
                     </span>
                 </button>
             </div>
-            <h2>Select Delivery Option</h2><br/>
+            <h2>Select Delivery Option</h2><br />
             <h5>Standard Delivery</h5>
-            <input type='radio' value='Standard Delivery' name='delivery' checked={true}/>
+            <input type='radio' value='Standard Delivery' name='delivery' checked={true} />
             <span> Delivered by: DD/MM/YY - DD/MM/YY </span>
             <span>$ {deliveryFee}</span>
-            <br/>Select time preference:
+            <br />Select time preference:
             <button> 9AM - 1PM </button> <button> 1PM - 5PM </button>
-            <hr/>
+            <hr />
 
             <h5>Express Delivery</h5>
             <input type='radio' value='Standard Delivery' name='delivery' />
             <span> Delivered by: DD/MM/YY - DD/MM/YY </span>
             <span>$ {deliveryFee}</span>
-            <br/>Select time preference:
-            <button> 9AM - 1PM </button> <button> 1PM - 5PM </button> <br/><br/>
-            <hr/>
+            <br />Select time preference:
+            <button> 9AM - 1PM </button> <button> 1PM - 5PM </button> <br /><br />
+            <hr />
             <div>Amount: ${calculateTotal()}</div>
             <div>Total amount: ${(parseFloat(calculateTotal()) + deliveryFee).toFixed(2)}</div>
 
-            <input type='text' placeholder='Add a note '/>
+            <input type='text' placeholder='Add a note ' />
 
             <div className='form-group' align='center'>
-                <br/>
-                <button className='checkoutButton' 
+                <br />
+                <button className='checkoutButton'
                     onClick={() => navigate('/payment', { state: { deliveryFee: deliveryFee } })}>
                     Continue to Payment
                 </button>
             </div>
 
         </div>
-      );
+    );
 }
