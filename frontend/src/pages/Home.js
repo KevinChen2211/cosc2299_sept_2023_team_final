ad0591d9181e9df961ea5a99ab390ca92310291d
s3780646 <s3780646@student.rmit.edu.au>
2023-10-08 03:58:51 +1100

    Task 104 edit final css touch

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -26,27 +26,29 @@ function Home(props) {
             className="home-banner"
           />
           <br /><br /><br />
-          <p>
-            This project is a collaborative effort aimed to develop a
-            comprehensive price-matching and delivery application. With
-            SuperPrice, we can help you find the best value for your money's
-            worth by comparing prices across multiple retailers and also arrange
-            for a seamless delivery.
-          </p>
-          <br />
-          <br />
-          <p>
-            By integrating with various local supermarkets, this application
-            uses real-time data to ensure accurate and up-to-date information
-            about product prices and availability.
-          </p>
+          <div className="home-description">
+            <p>
+              This project is a collaborative effort aimed to develop a
+              comprehensive price-matching and delivery application. With
+              SuperPrice, we can help you find the best value for your money's
+              worth by comparing prices across multiple retailers and also arrange
+              for a seamless delivery.
+            </p>
+            <br />
+            <p>
+              By integrating with various local supermarkets, this application
+              uses real-time data to ensure accurate and up-to-date information
+              about product prices and availability.
+            </p>
+            <br />
+            <p>
+              Create an account with us today to improve your shopping experience,
+              save both time and money, and simplify the grocery shopping process!
+            </p>
+            <br />
+            <p>You'll get exclusive access to source the best grocery products for incredibly low prices.</p>
+          </div>
           <br /><br />
-          <p>
-            Create an account with us today to improve your shopping experience,
-            save both time and money, and simplify the grocery shopping process!
-          </p>
-          <br /><br />
-          <p>You'll get exclusive access to source the best grocery products for incredibly low prices.</p>
           <button onClick={handleSubmit} className="button-interactive">
             Sign Up
           </button>
