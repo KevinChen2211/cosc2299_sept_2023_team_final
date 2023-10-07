06e7e394c6b76670832fe496a06fa5b3cf73eeea
s3780646 <s3780646@student.rmit.edu.au>
2023-10-08 02:30:48 +1100

    Task 104 add button stylings

--- frontend/src/pages/ShoppingCart.js
+++ frontend/src/pages/ShoppingCart.js
@@ -109,7 +109,7 @@ export default function ShoppingCart({ cartItems, updateCartItems }) {
         <div className="popup">
           <h2>Zero Total Warning</h2>
           <p>Your total is $0.00. You cannot proceed to checkout with an empty cart.</p>
-          <button onClick={() => setIsPopupOpen(false)}>Close</button>
+          <button className='default-home-button' onClick={() => setIsPopupOpen(false)}>Close</button>
         </div>
       </Popup>
     </div>
