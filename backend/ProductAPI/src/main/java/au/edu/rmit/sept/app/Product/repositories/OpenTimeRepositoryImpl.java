988daa0873c6018480a89583444b0a58b6b11010
bakainusuke <nhathuy520835@gmail.com>
2023-10-07 16:25:21 +1100

    update documentation for repo

--- backend/ProductAPI/src/main/java/au/edu/rmit/sept/app/Product/repositories/OpenTimeRepositoryImpl.java
+++ backend/ProductAPI/src/main/java/au/edu/rmit/sept/app/Product/repositories/OpenTimeRepositoryImpl.java
@@ -12,6 +12,20 @@ import au.edu.rmit.sept.app.Product.models.OpeningTime;
 
 @Repository
 public class OpenTimeRepositoryImpl implements OpenTimeRepository{
+
+    /**
+     * Retrieves opening times for a specific store or chain based on its name from
+     * an external service.
+     *
+     * @param name The name of the store or chain for which the opening times are to
+     *             be retrieved.
+     *
+     * @return List<OpeningTime> Returns a list of opening times for the specified
+     *         name.
+     *         If the opening times for the specified name are not found or an error
+     *         occurs,
+     *         an empty list or null is returned.
+     */
     @Override
     public List<OpeningTime> getByName(String name){
          RestTemplate restTemplate = new RestTemplate();
