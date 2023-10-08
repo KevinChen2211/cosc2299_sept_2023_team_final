dfa960aae82a05dbb42c38bcdd9a1440d2397ea1
s3780646 <s3780646@student.rmit.edu.au>
2023-10-08 19:36:30 +1100

    Task 104 create product components

---
+++ frontend/src/pages/components/ProductComponent.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function ProductComponent({ product }) {
+    const navigate = useNavigate();
+
+    const handleImageClick = (productIdentifier) => {
+        navigate(`/product/${product.name}`, { state: { productID: productIdentifier } });
+    };
+
+    return (
+        <div
+            key={product.productID}
+            className='products'
+            onClick={() => handleImageClick(product.productID)}
+        >
+            <img src={product.imageLocation} alt={product.name} width="100" />
+            <br />
+            <strong>{product.name}</strong>
+            <br />
+            ${product.price.toFixed(2)} AUD
+            <br />
+            Sold by: {product.chain}
+        </div>
+    );
+}
